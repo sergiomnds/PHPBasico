9be2a1bb371196b9e7f3ed8d9b4ca56d1e7e6743
Sérgio Mendes <85349959+sergiomnds@users.noreply.github.com>
2023-02-23 14:43:00 -0300

    arquivos criados durante o curso

---
+++ cursophp/anotacoes/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis e Constantes</title>
+</head>
+<body>
+    <h1>Trabalhando com variaveis e constantes</h1>
+    <?php
+        $nome = "Pedro";
+        $Nome = "Maria"; //Case sensitive
+        echo "É verdade que seu nome é $nome? O nome de outra pessoa é $Nome.";
+
+        $salario = 2512.50;
+        echo " Seu salário é $salario";
+    ?>
+</body>
+</html>
